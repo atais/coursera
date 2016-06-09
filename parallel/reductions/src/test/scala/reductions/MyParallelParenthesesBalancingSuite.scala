3782823c0ad4abbf159b447351862e7368647a14
Michał Siatkowski <msiatkowski@dotsystems.pl>
2016-06-09 17:07:11 +0200

    after submit :(

--- parallel/reductions/src/test/scala/reductions/MyParallelParenthesesBalancingSuite.scala
+++ parallel/reductions/src/test/scala/reductions/MyParallelParenthesesBalancingSuite.scala
@@ -41,13 +41,24 @@ class MyParallelParenthesesBalancingSuite extends FunSuite {
     check(").", false)
   }
 
-  test("jaceks example") {
+  test("jaceks example 2") {
     def check(input: String, expected: Boolean) =
       assert(parBalance(input.toArray, 2) == expected,
         s"balance($input) should be $expected")
 
     check("(())()()", true)
     check("(())))((", false)
+    check("(())", true)
+  }
+
+  test("jaceks example 1") {
+    def check(input: String, expected: Boolean) =
+      assert(parBalance(input.toArray, 1) == expected,
+        s"balance($input) should be $expected")
+
+    check("(())()()", true)
+    check("(())))((", false)
+    check("(())", true)
   }
 
 
