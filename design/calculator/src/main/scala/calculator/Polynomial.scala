d42c43cde4c8db379b0a2dfad94bad7781806021
Michał Siatkowski <msiatkowski@dotsystems.pl>
2016-06-30 17:37:08 +0200

    Polynomials??

--- design/calculator/src/main/scala/calculator/Polynomial.scala
+++ design/calculator/src/main/scala/calculator/Polynomial.scala
@@ -2,12 +2,22 @@ package calculator
 
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+                   c: Signal[Double]): Signal[Double] = {
+    Signal(math.pow(b(), 2) - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+                       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    val delta = computeDelta(a, b, c)
+    if (delta() > 0) {
+      Signal(Set(
+        (-b() + math.sqrt(delta())) / (2 * a()),
+        (-b() - math.sqrt(delta())) / (2 * a())
+      ))
+    } else if (delta() == 0) {
+      Signal(Set(-b() / (2 * a())))
+    } else {
+      Signal(Set.empty)
+    }
   }
 }
