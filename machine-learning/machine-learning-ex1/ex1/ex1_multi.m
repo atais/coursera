baa7f62d72ad3ec568b24fc0b034007ac67c750c
Michał Siatkowski <atais.jr@gmail.com>
2016-11-09 23:49:25 +0100

    Ex1 done 100%

--- machine-learning/machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning/machine-learning-ex1/ex1/ex1_multi.m
@@ -104,7 +104,11 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
+
+house = bsxfun(@rdivide, [1650, 3] - mu, sigma);
+house = [1 house]; 
+
+price = house * theta;
 
 
 % ============================================================
@@ -149,7 +153,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = house * theta;
 
 
 % ============================================================
--- machine-learning/machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning/machine-learning-ex1/ex1/gradientDescent.m
@@ -8,7 +8,7 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
-
+  
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
     %               theta. 
@@ -17,11 +17,7 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
-
-
-
-
-
+    theta = theta - ( alpha * ( 1 / m ) * (( X * theta - y )' * X )' );
 
     % ============================================================
 
