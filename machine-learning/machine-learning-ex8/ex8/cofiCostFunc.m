d866074108e29bf96ea72109b7efbea0a12d4064
Michał Siatkowski <msiatkowski@dotsystems.pl>
2016-12-23 11:19:58 +0100

    ex8 100%

--- machine-learning/machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,20 +40,13 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+error = (((X * Theta') - Y) .* R).^2;
+p1 = lambda/2 * sum(sum( (Theta).^2 ));
+p2 = lambda/2 * sum(sum( (X).^2 ));
+J = 1/2 * sum(sum(error)) + p1 + p2;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+X_grad = ((((X * Theta') - Y) .* R) * Theta) .+ (lambda * X);
+Theta_grad = ((((X * Theta') - Y) .* R)' * X) .+ (lambda * Theta);
 
 % =============================================================
 
