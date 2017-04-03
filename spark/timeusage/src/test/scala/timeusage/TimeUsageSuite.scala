32aca9cf34b2534f84b1babecae3ff6774c685f3
Michał Siatkowski <atais.jr@gmail.com>
2017-04-04 00:11:11 +0200

    6/10 wip

--- spark/timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
+++ spark/timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
@@ -1,19 +1,45 @@
 package timeusage
 
-import org.apache.spark.sql.{ColumnName, DataFrame, Row}
-import org.apache.spark.sql.types.{
-  DoubleType,
-  StringType,
-  StructField,
-  StructType
-}
+import org.apache.spark.sql.{Column, ColumnName, DataFrame, Row}
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.{BeforeAndAfterAll, FunSuite}
+import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
 
 import scala.util.Random
 
 @RunWith(classOf[JUnitRunner])
-class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
+class TimeUsageSuite extends FunSuite with Matchers with BeforeAndAfterAll {
+
+  test("dfSchema") {
+    val schema = TimeUsage.dfSchema(List("a", "b", "c"))
+    val expected = new StructType()
+      .add("a", StringType, false)
+      .add("b", DoubleType, false)
+      .add("c", DoubleType, false)
+
+    schema shouldEqual expected
+  }
+
+  test("row") {
+    val row = TimeUsage.row(List("a", "0.5", "1"))
+    val expected = Row("a", 0.5d, 1d)
+
+    row shouldEqual expected
+  }
+
+  test("classified columns") {
+    /*
+    * 1. “primary needs” activities (sleeping, eating, etc.). These are the columns starting with “t01”, “t03”, “t11”,
+    *      “t1801” and “t1803”.
+    * 2. working activities. These are the columns starting with “t05” and “t1805”.
+    * 3. other activities (leisure). These are the columns starting with “t02”, “t04”, “t06”, “t07”, “t08”, “t09”,
+    *      “t10”, “t12”, “t13”, “t14”, “t15”, “t16” and “t18” (those which are not part of the previous groups only).
+    */
+    val names = List("t0102", "t0301", "t110012", "t1801123132", "t1803132123")
+    val classified = TimeUsage.classifiedColumns(names)
+
+    classified._1 shouldEqual names.map(new Column(_))
+  }
 
 }
