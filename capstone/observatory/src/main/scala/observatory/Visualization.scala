2c933e5eb1f03ffad79a191e55b1f185e9f08f1a
Michał Siatkowski <atais.jr@gmail.com>
2017-04-11 00:49:49 +0200

    working on predictTemperature

--- capstone/observatory/src/main/scala/observatory/Visualization.scala
+++ capstone/observatory/src/main/scala/observatory/Visualization.scala
@@ -1,6 +1,6 @@
 package observatory
 
-import com.sksamuel.scrimage.{Image, Pixel}
+import com.sksamuel.scrimage.Image
 
 /**
   * 2nd milestone: basic visualization
@@ -9,16 +9,43 @@ object Visualization {
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
-    * @param location Location where to predict the temperature
+    * @param location     Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
-    ???
+    val (v1, v2) = temperatures.map { case (l, t) =>
+      val gcDist = gcDistance(l, location)
+      if (gcDist <= 1) {
+        (t, 0d)
+      } else {
+        val dist = math.pow(1.0 / gcDist, 2)
+        (dist * t, dist)
+      }
+    }.fold((0d, 0d)) { case ((agg1, agg2), (x, y)) => (agg1 + x, agg2 + y) }
+
+    val nonZero = if (v2 == 0) 1 else v2
+    v1 / nonZero
+  }
+
+  private[observatory] def gcDistance(l1: Location, l2: Location): Double = {
+    import math._
+    val R = 6371e3
+    val φ1 = l1.lat.toRadians
+    val λ1 = l1.lon.toRadians
+    val φ2 = l2.lat.toRadians
+
+    val λ2 = l2.lon.toRadians
+    val Δφ = φ2 - φ1
+    val Δλ = λ2 - λ1
+
+    val a = sin(Δφ / 2) * sin(Δφ / 2) + cos(φ1) * cos(φ2) * sin(Δλ / 2) * sin(Δλ / 2)
+    val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    R * c / 1000
   }
 
   /**
     * @param points Pairs containing a value and its associated color
-    * @param value The value to interpolate
+    * @param value  The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
@@ -27,7 +54,7 @@ object Visualization {
 
   /**
     * @param temperatures Known temperatures
-    * @param colors Color scale
+    * @param colors       Color scale
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)]): Image = {
--- capstone/observatory/src/test/scala/observatory/VisualizationTest.scala
+++ capstone/observatory/src/test/scala/observatory/VisualizationTest.scala
@@ -2,12 +2,31 @@ package observatory
 
 
 import org.junit.runner.RunWith
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Matchers}
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.prop.Checkers
 
 @RunWith(classOf[JUnitRunner])
-class VisualizationTest extends FunSuite with Checkers {
+class VisualizationTest extends FunSuite with Checkers with Matchers {
 
+
+  test("great-circle distance") {
+    val loc1 = Location(52.205, 0.119)
+    val loc2 = Location(48.857, 2.351)
+
+    val res = Visualization.gcDistance(loc1, loc2)
+
+    BigDecimal(res).setScale(1, BigDecimal.RoundingMode.HALF_EVEN) shouldEqual 404.3
+  }
+
+  test("simple predict") {
+    val loc = Location(52.205, 0.119)
+    val testValue = 10d
+    val val1 = Seq((loc, testValue))
+
+    val res = Visualization.predictTemperature(val1, loc)
+
+    res shouldEqual testValue
+  }
 
 }
