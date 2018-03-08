5db41e6f1d16e15f66c0072be917f93e5d647e5a
Michał Siatkowski <michal.siatkowski@cortb.pl>
2018-03-08 15:39:01 +0100

    week 2 wip

--- capstone/observatory/src/main/scala/observatory/Visualization.scala
+++ capstone/observatory/src/main/scala/observatory/Visualization.scala
@@ -2,6 +2,8 @@ package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
 
+import scala.util.{Failure, Success, Try}
+
 /**
   * 2nd milestone: basic visualization
   */
@@ -9,29 +11,101 @@ object Visualization {
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
-    * @param location Location where to predict the temperature
+    * @param location     Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-    ???
+    def wix(x: Location, xi: (Location, Temperature)): (Temperature, Double) = {
+      val (xil, xit) = xi
+      val gcDist = gcDistance(x, xil)
+      if (gcDist <= 1) {
+        (xit, 1d)
+      } else {
+        val dist = 1.0 / math.pow(gcDist, 2)
+        (dist * xit, dist)
+      }
+    }
+
+    val (v1, v2) = temperatures
+      .map(wix(location, _))
+      .fold((0d, 0d)) { case ((agg1, agg2), (x, y)) => (agg1 + x, agg2 + y) }
+
+    val nonZero = if (v2 == 0) 1 else v2
+    v1 / nonZero
+  }
+
+  private[observatory] def gcDistance(l1: Location, l2: Location): Double = {
+    import math._
+    val R = 6371
+    val φ1 = l1.lat.toRadians
+    val λ1 = l1.lon.toRadians
+    val φ2 = l2.lat.toRadians
+    val λ2 = l2.lon.toRadians
+
+    val Δφ = φ2 - φ1
+    val Δλ = λ2 - λ1
+
+    val Δδ = if (l1 == l2) {
+      0
+    } else if (l1.lat == -l2.lat && l1.lon == -l2.lon) {
+      Pi
+    } else {
+      acos(sin(φ1) * sin(φ2) + cos(φ1) * cos(φ2) * cos(Δλ))
+    }
+    R * Δδ
   }
 
   /**
     * @param points Pairs containing a value and its associated color
-    * @param value The value to interpolate
+    * @param value  The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color = {
-    ???
+    val pMap = points.toMap
+    val keys = pMap.keys.toSeq.sorted
+
+    val x1 = keys.find(_ >= value).getOrElse(keys.max)
+    val x0 = keys.reverse.find(_ <= value).getOrElse(keys.min)
+
+    val y1 = pMap(x1)
+    val y0 = pMap(x0)
+
+    if (x0 == x1) y0
+    else {
+      val y1d = ColorD(y1)
+      val y0d = ColorD(y0)
+      Try {
+        (y0d + ((y1d - y0d) / (x1 - x0) * (value - x0))).normalize
+      } match {
+        case Success(c) => c
+        case Failure(t) =>
+          println(t.toString)
+          Color(0,0,0)
+      }
+    }
   }
 
   /**
     * @param temperatures Known temperatures
-    * @param colors Color scale
+    * @param colors       Color scale
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)]): Image = {
-    ???
+    val (w, h) = (360, 180)
+
+    val pixels = for {
+      x <- 0 until w
+      y <- 0 until h
+    } yield {
+      val lat = h / 2 - y
+      val lon = -1 * (w / 2 - x)
+      val loc = Location(lat, lon)
+      val temp = predictTemperature(temperatures, loc)
+      val col = interpolateColor(colors, temp)
+      Pixel(col.red, col.green, col.blue, 1)
+    }
+
+    Image(w, h, pixels.toArray)
   }
 
 }
--- capstone/observatory/src/main/scala/observatory/models.scala
+++ capstone/observatory/src/main/scala/observatory/models.scala
@@ -2,12 +2,15 @@ package observatory
 
 import java.time.LocalDate
 
+import scala.math.BigDecimal.RoundingMode
+
 case class WeatherStation(stn: String, wban: String, loc: Location)
 
 case class TemperatureC(stn: String, wban: String, date: LocalDate, temp: Double)
 
 /**
   * Introduced in Week 1. Represents a location on the globe.
+  *
   * @param lat Degrees of latitude, -90 ≤ lat ≤ 90
   * @param lon Degrees of longitude, -180 ≤ lon ≤ 180
   */
@@ -17,8 +20,9 @@ case class Location(lat: Double, lon: Double)
   * Introduced in Week 3. Represents a tiled web map tile.
   * See https://en.wikipedia.org/wiki/Tiled_web_map
   * Based on http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
-  * @param x X coordinate of the tile
-  * @param y Y coordinate of the tile
+  *
+  * @param x    X coordinate of the tile
+  * @param y    Y coordinate of the tile
   * @param zoom Zoom level, 0 ≤ zoom ≤ 19
   */
 case class Tile(x: Int, y: Int, zoom: Int)
@@ -26,6 +30,7 @@ case class Tile(x: Int, y: Int, zoom: Int)
 /**
   * Introduced in Week 4. Represents a point on a grid composed of
   * circles of latitudes and lines of longitude.
+  *
   * @param lat Circle of latitude in degrees, -89 ≤ lat ≤ 90
   * @param lon Line of longitude in degrees, -180 ≤ lon ≤ 179
   */
@@ -33,6 +38,7 @@ case class GridLocation(lat: Int, lon: Int)
 
 /**
   * Introduced in Week 5. Represents a point inside of a grid cell.
+  *
   * @param x X coordinate inside the cell, 0 ≤ x ≤ 1
   * @param y Y coordinate inside the cell, 0 ≤ y ≤ 1
   */
@@ -40,9 +46,49 @@ case class CellPoint(x: Double, y: Double)
 
 /**
   * Introduced in Week 2. Represents an RGB color.
-  * @param red Level of red, 0 ≤ red ≤ 255
+  *
+  * @param red   Level of red, 0 ≤ red ≤ 255
   * @param green Level of green, 0 ≤ green ≤ 255
-  * @param blue Level of blue, 0 ≤ blue ≤ 255
+  * @param blue  Level of blue, 0 ≤ blue ≤ 255
   */
 case class Color(red: Int, green: Int, blue: Int)
+
+case class ColorD(red: Double, green: Double, blue: Double) {
+
+  def +(o: ColorD): ColorD = this ++ o
+
+  def -(o: ColorD): ColorD = this ++ (o ** (-1))
+
+  def *(d: Double): ColorD = this ** d
+
+  def /(d: Double): ColorD = this ** (1 / d)
+
+  private def ++(o: ColorD): ColorD = {
+    val r = red + o.red
+    val g = green + o.green
+    val b = blue + o.blue
+    ColorD(r, g, b)
+  }
+
+  private def **(d: Double): ColorD = {
+    val r = red * d
+    val g = green * d
+    val b = blue * d
+    ColorD(r, g, b)
+  }
+
+  def normalize: Color = Color(calmp(red * 255), calmp(green * 255), calmp(blue * 255))
+
+  private def calmp(i: BigDecimal) = {
+    if (i > 255) 255
+    else if (i < 0) 0
+    else i.setScale(0, RoundingMode.HALF_UP).toIntExact
+  }
+
+}
+
+object ColorD {
+  def apply(c: Color): ColorD = new ColorD(c.red / 255, c.green / 255, c.blue / 255)
+}
+
 
